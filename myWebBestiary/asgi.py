3de192baf732dd6b951f79defc5d4b829d919fb8
Xenvar2307 <136366607+Xenvar2307@users.noreply.github.com>
2026-01-05 18:52:29 +0100

    Tutorial part1

---
+++ myWebBestiary/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for myWebBestiary project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myWebBestiary.settings')
+
+application = get_asgi_application()
